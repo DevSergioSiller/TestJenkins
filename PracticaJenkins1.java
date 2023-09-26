a6903b5236c7bb7a90abfcdcb7ebcccbc565c11e
SergioSiller <ssiller_mena@hotmail.com>
2023-09-26 11:39:34 -0600

    Hello World

---
+++ PracticaJenkins1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicajenkins1;
+
+/**
+ *
+ * @author sergio
+ */
+public class PracticaJenkins1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+
+        System.out.println("A commit has been made");
+
+
+
+
+
+    }
+    
+}
