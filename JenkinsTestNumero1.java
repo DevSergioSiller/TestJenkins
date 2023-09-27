239df0bdac24cb7282e0bf03f749440de7a6ad7d
SergioSiller <ssiller_mena@hotmail.com>
2023-09-26 19:13:20 -0600

    testeo

--- JenkinsTestNumero1.java
+++ JenkinsTestNumero1.java
@@ -3,21 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package practicajenkins1;
+package jenkinstestnumero1;
 
 /**
  *
  * @author sergio
  */
-public class PracticaJenkins1 {
+public class JenkinsTestNumero1 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-                System.out.println("Hola, Jenkins. ¡Estoy funcionando!");
-
+                System.out.println("Hola, Jenkins. Â¡Estoy funcionando!");
     }
     
 }
